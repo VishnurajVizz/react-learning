010c61389d1124544649e5510aa8365b8762e9cb
Vishnuraj <vishnuraj.arjunasamy@cdw.com>
2024-09-17 00:02:35 +0530

    Add player name and symbol

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import React from "react";
+import Player from "./components/player";
+
 function App() {
   return (
     <>
-      <h1>React Tic-Tac-Toe</h1>
+      <header>
+        <img src="game-logo.png" />
+        <h1>Tic-Tac-Toe</h1>
+      </header>
+      <main>
+        <div id="game-container">
+          <ol id="players">
+            <Player pName={"Player 1"} symbol={"X"} />
+            <Player pName={"Player 2"} symbol={"O"} />
+          </ol>
+          Game Board
+        </div>
+        LOG
+      </main>
     </>
   );
 }
---
+++ src/components/Player.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useState } from "react";
+import Button from "./Button";
+import Input from "./Input";
+export default function Player({ pName = "", symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [name, setName] = useState(pName);
+  let playerName = <span className="player-name">{name}</span>;
+  let btn = "Edit";
+  if (isEditing) {
+    playerName = (
+      <Input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+    );
+    btn = "Save";
+  }
+  return (
+    <li>
+      <span className="player">
+        {playerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <Button
+        onClick={() => {
+          setIsEditing(prev=>!prev);
+        }}
+      >
+        {btn}
+      </Button>
+    </li>
+  );
+}
