72a9a52d9ccc7dbff4a28a88ed8c717e7d6bd12c
Vishnuraj <vishnuraj.arjunasamy@cdw.com>
2024-09-19 14:25:08 +0530

    browser router basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
 import { useState } from "react";
+import { Route, Routes, Link } from "react-router-dom";
 import "./App.css";
+import Home from "./components/home";
+import BookList from "./components/bookList";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <nav>
+        <ol>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/books">Books</Link>
+          </li>
+        </ol>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books" element={<BookList />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
