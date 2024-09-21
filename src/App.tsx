4c20e0a86ad1ef11ac99909f0f6a1a99491ccb2c
Vishnuraj <vishnuraj.arjunasamy@cdw.com>
2024-09-21 08:46:49 +0530

    Declaring varibales in Ts

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,18 @@ import './App.css'
 
 function App() {
 
+  let name:string = 'Vizz'
+  let age:number=23
+  let isActive:boolean=true
+  let ratings:number[] = [3,4,5,6]
+
+  let person:any = 3
+  person="Hi"
+  
+
   return (
     <>
-      
+      {name} {age} {isActive} {ratings} {person}
     </>
   )
 }
